db2736de21a68186a99eb85f12986a1b53ab27ae
SeresSchaeffer <92908901+SeresSchaeffer@users.noreply.github.com>
2022-05-21 01:36:28 +0700

    navbar

---
+++ nav.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/nav.css">
+
+  <nav class="navbar navbar-expand navbar-light" style="background-color: #ffdda8  ; z-index: 9;" id="nav">
+    <div class="container">
+      <a class="navbar-brand" href="index.php"><font color=#a06300 ><i class="bi bi-house-door-fill"></i> EnET888</font></a>
+      <a class="navbar-brand" href="market.php"><font color=#a06300 ><i class="bi bi-cart-check"></i> Cart</font></a>
+      <a class="navbar-brand" href="login.php"><font color=#a06300 ><i class="bi bi-box-arrow-in-right"></i> login</font></a>
+      <div id="indicator"></div>
+    </div>
+  </nav> 
