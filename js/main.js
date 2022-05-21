09aae3ddd90523e0c66541b8d1c36abd8170c194
lukwa999 <92910988+lukwa999@users.noreply.github.com>
2022-05-22 01:59:38 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
 (function($) {
     "use strict";
 
-    // Dropdown on mouse hover
-    $(document).ready(function() {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function() {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function() {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-
-
     // Back to top button
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
@@ -31,34 +13,6 @@
         $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-
-
-    // Vendor carousel
-    $('.vendor-carousel').owlCarousel({
-        loop: true,
-        margin: 29,
-        nav: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        responsive: {
-            0: {
-                items: 2
-            },
-            576: {
-                items: 3
-            },
-            768: {
-                items: 4
-            },
-            992: {
-                items: 5
-            },
-            1200: {
-                items: 6
-            }
-        }
-    });
-
 
     // Related carousel
     $('.related-carousel').owlCarousel({
@@ -82,7 +36,6 @@
             }
         }
     });
-
 
     // Product Quantity
     $('.quantity button').on('click', function() {
